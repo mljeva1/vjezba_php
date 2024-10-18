73b6220bdeed1a0e127cb257f88c52baf897f87a
Mateo <121026384+mljeva1@users.noreply.github.com>
2024-10-18 17:41:14 +0200

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,21 @@
+<?php
+// PHP blok počinje na početku dokumenta prije deklaracije
+echo "<!DOCTYPE html>";
+echo "<html lang='hr'>";
+
+echo "<head>";
+echo "<meta charset='UTF-8'>";
+echo "<meta name='description' content='Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.'>";
+echo "<meta name='keywords' content='Da Vincijev kod, kriminalistički triler, Dan Brown'>";
+echo "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+echo "<title>Da Vincijev kod</title>";
+echo "</head>";
+
+echo "<body>";
+echo "<h1>Da Vincijev kod</h1>";
+echo "<p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>";
+echo "<a href='https://hr.wikipedia.org/Da_Vincijev_kod' target='_blank'>Više informacija o Da Vincijevom kodu</a>";
+echo "</body>";
+
+echo "</html>";
+?>
